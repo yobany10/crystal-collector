29c3b1486d1e3c9889cf146293d1cc3a907e0082
Yobany <yobanydesign@gmail.com>
2019-03-17 22:08:25 -0400

    Added functions to handle the clicks for the crystals. Updated CSS styling.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,92 @@
 $(document).ready(function () {
-    var Random = Math.floor(Math.random() * 121 + 19)
-    $('#randomNum').text(Random)
+    var Random = Math.floor(Math.random() * 127 + 21)
+    $('#randomNum').text("Match this: " + Random);
 
     var generatedNumbers = []
     var total = 0;
     var wins = 0;
     var losses = 0;
-    $('#userWins').text(`Wins: ${wins}`);
-    $('#userLosses').text(`Losses: ${losses}`);
+    $('#userWins').text("Wins: " + wins);
+    $('#userLosses').text("Losses: " + losses);
 
     function crystalNumbers() {
         for (i=0; i < 4; i++) {
-            var num = Math.floor(Math.random() * 7 + 1);
+            var num = Math.floor(Math.random() * 10 + 5);
             generatedNumbers.push(num);
         }
-    }
+    };
     crystalNumbers();
 
     function resetGame() {
-        Random = Math.floor(Math.random() * 23 + 5);
-        $('#randomNum').text(Random);
-        GeneratedNumbers = [];
-        fourRandomNumbers();
+        Random = Math.floor(Math.random() * 127 + 21);
+        $('#randomNum').text("Match this: " + Random);
+        generatedNumbers = [];
+        crystalNumbers();
         total = 0;
-        $('#userScore').text(`Your number: ${score}`);
-    }
+        $('#userScore').text("Your number: " + total);
+    };
 
     function win() {
         alert("Congrats, you won!");
         wins++;
-        $('#userWins').text(wins);
-        reset();
-    }
+        $('#userWins').text("Wins: " + wins);
+        resetGame();
+    };
 
     function lose() {
         alert("Sorry, you just lost.");
         losses++;
-        $('#userLosses').text(losses);
-        reset();
-    }
+        $('#userLosses').text("Losses: " + losses);
+        resetGame();
+    };
+
+    $('#crystalOne').on('click', function () {
+        total = total + generatedNumbers[0];
+        $('#userNum').text("Your number: " + total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    });
+
+    $('#crystalTwo').on('click', function () {
+        total = total + generatedNumbers[1];
+        $('#userNum').text("Your number: " + total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    });
+
+    $('#crystalThree').on('click', function () {
+        total = total + generatedNumbers[2];
+        $('#userNum').text("Your number: " + total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    });
+
+    $('#crystalFour').on('click', function () {
+        total = total + generatedNumbers[3];
+        $('#userNum').text("Your number: " + total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    });
 
 
-})
+});
